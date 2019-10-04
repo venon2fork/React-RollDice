0b611ebc80ad02cd5311afcb9cd7e53f96c5add1
venon2fork <venon2fork@gmail.com>
2019-10-04 22:45:33 +0200

    React DiceRoll

---
+++ src/Die.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './Die.css'
+
+export class Die extends Component {
+    render() {
+        
+        return (
+            
+               <i
+        className={`Die fas fa-dice-${this.props.face} ${this.props.rolling &&
+          "shaking"}`}
+      />
+            
+        )
+    }
+}
+
+export default Die
